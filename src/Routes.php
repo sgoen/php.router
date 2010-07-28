5923e2df0eaf4de4af631420eacc34ae8787590e
J.Smit <j.smit@sgoen.nl>
2010-07-28 11:38:10 +0200

    updated comment for routes array

--- src/Routes.php
+++ src/Routes.php
@@ -7,6 +7,9 @@
 class Routes
 {
 	/**
+	 * The order of routes is important because when the router matches an url to the routes
+	 * it will start with the LAST one. The first match will be used.
+     *
 	 * @var array() $routes, holds a multidimensional array of routes.
 	 */
 	public static $routes = array(
